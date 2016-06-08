755ee5e08968188ae09104fe0f84b074a02faf26
Mauri de Souza Nunes <mauri870@gmail.com>
2016-06-08 11:56:16 -0300

    Add task for styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
+var sass = require('gulp-sass');
 
 gulp.task('scripts', function () {
    return gulp.src(['app/js/app.js'])
@@ -7,4 +8,10 @@ gulp.task('scripts', function () {
        .pipe(gulp.dest('build/js/'));
 });
 
-gulp.task('default', ['scripts']);
+gulp.task('styles', function () {
+   return gulp.src(['app/sass/app.scss'])
+       .pipe(sass())
+       .pipe(gulp.dest('build/css/'));
+});
+
+gulp.task('default', ['scripts','styles']);
