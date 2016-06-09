44cf2a486c50f6f77d2eaa0126fba7e306f7d087
Mauri de Souza Nunes <mauri870@gmail.com>
2016-06-09 14:58:35 -0300

    Fix path to index.html

--- app/main.js
+++ app/main.js
@@ -13,7 +13,7 @@ function createWindow () {
   mainWindow = new BrowserWindow({width: 1024, height: 768})
 
   // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/build/index.html`)
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
 
   // Open the DevTools.
   //mainWindow.webContents.openDevTools()
